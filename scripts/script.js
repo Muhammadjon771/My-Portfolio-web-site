21ed77c70927224dcd5654bf7f3f4d3be699d2ac
Aslmov Muhammadjon <muhammadzon329@gmail.com>
2024-10-08 10:56:10 +0300

    добавил анимацию печати текста js

--- scripts/script.js
+++ scripts/script.js
@@ -64,3 +64,66 @@ toggleThemeButton.addEventListener('click', () => {
 });
 
   
+
+const text1 = "Aslamov Muhammadjon";
+const text2 = "a Front-End Developer";
+const text3 = "a Student"
+const textElement = document.getElementById("text");
+let index1 = 0;
+let index2 = 0;
+let index3 = 0;
+
+// Функция для печати первого текста
+function typeText1() {
+    if (index1 < text1.length) {
+        textElement.innerHTML += text1.charAt(index1);
+        index1++;
+        setTimeout(typeText1, 100); // Задержка между буквами (100 мс)
+    } else {
+        setTimeout(deleteText1, 500); // Пауза перед удалением текста
+    }
+}
+
+// Функция для удаления текста по одной букве
+function deleteText1() {
+    if (index1 >= 0) {
+        textElement.innerHTML = text1.substring(0, index1);
+        index1--;
+        setTimeout(deleteText1, 100); // Задержка между удалением букв (100 мс)
+    } else {
+        setTimeout(typeText2, 500); // Пауза перед началом второго текста
+    }
+}
+
+// Функция для печати второго текста
+function typeText2() {
+    if (index2 < text2.length) {
+        textElement.innerHTML += text2.charAt(index2);
+        index2++;
+        setTimeout(typeText2, 100); // Задержка между буквами (100 мс)
+    }
+    else {
+      setTimeout(deleteText2, 500); // Пауза перед удалением текста
+  }
+}
+
+function deleteText2() {
+  if (index2 >= 0) {
+      textElement.innerHTML = text2.substring(0, index2);
+      index2--;
+      setTimeout(deleteText2, 100); // Задержка между удалением букв (100 мс)
+  } else {
+      setTimeout(typeText3, 500); // Пауза перед началом второго текста
+  }
+}
+
+function typeText3() {
+  if (index3 < text3.length) {
+      textElement.innerHTML += text3.charAt(index3);
+      index3++;
+      setTimeout(typeText3, 100); // Задержка между буквами (100 мс)
+  }
+}
+
+typeText1(); // Начинаем анимацию
+
